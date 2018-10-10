31cead299cbf4254f58b09b5fb6ecea0f1b5cf8d
kacky24 <kakiuchi@crimson.q.t.u-tokyo.ac.jp>
2018-10-10 16:52:44 +0900

    update

--- scripts/data_prep.sh
+++ scripts/data_prep.sh
@@ -1,13 +1,16 @@
 
-NUM_WORDS=50001
-OUTPUT_DIR="TFRecords" 
-VOCAB_FILE="dictionary.txt"  
-TOKENIZED_FILES="BookCorpus/txt_tokenized/*"
+NUM_WORDS=20001
+OUTPUT_DIR="../src/data/TFRecords" 
+VOCAB_FILE="../src/data/dictionary.txt"  
+TOKENIZED_FILES="../src/data/txt_tokenized/*"
 
-python src/data/preprocess_dataset.py \
+python ../src/data/preprocess_dataset.py \
   --input_files "$TOKENIZED_FILES" \
-  --vocab_file $VOCAB_FILE \
   --output_dir $OUTPUT_DIR \
   --num_words $NUM_WORDS \
   --max_sentence_length 50 \
   --case_sensitive
+
+
+
+#   --vocab_file $VOCAB_FILE \
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,9 @@
 
-RESULTS_HOME="results"
-MDL_CFGS="model_configs"
+RESULTS_HOME="../src/data/results"
+MDL_CFGS="../model_configs"
 GLOVE_PATH="dictionaries"
 
-DATA_DIR="data/BC/TFRecords"
+DATA_DIR="../src/data/TFRecords"
 NUM_INST=45786400 # Number of sentences
 
 #DATA_DIR="data/BC_UMBC/TFRecords"
@@ -15,7 +15,7 @@ BS=400
 SEQ_LEN=30
 
 export CUDA_VISIBLE_DEVICES=0
-python src/train.py \
+python ../src/train.py \
     --input_file_pattern="$DATA_DIR/train-?????-of-00100" \
     --train_dir="$RESULTS_HOME/$CFG/train" \
     --learning_rate_decay_factor=0 \
@@ -24,15 +24,15 @@ python src/train.py \
     --nepochs=1 \
     --num_train_inst=$NUM_INST \
     --save_model_secs=1800 \
-    --Glove_path=$GLOVE_PATH \
-    --model_config="$MDL_CFGS/$CFG/train.json" &
+    --model_config="$MDL_CFGS/train.json" # &
+    # --Glove_path=$GLOVE_PATH \
 
-export CUDA_VISIBLE_DEVICES=1
-python src/eval.py \
-    --input_file_pattern="$DATA_DIR/validation-?????-of-00001" \
-    --checkpoint_dir="$RESULTS_HOME/$CFG/train" \
-    --eval_dir="$RESULTS_HOME/$CFG/eval" \
-    --batch_size=$BS \
-    --model_config="$MDL_CFGS/$CFG/train.json" \
-    --eval_interval_secs=1800 \
-    --sequence_length=$SEQ_LEN &
+# export CUDA_VISIBLE_DEVICES=1
+# python src/eval.py \
+#     --input_file_pattern="$DATA_DIR/validation-?????-of-00001" \
+#     --checkpoint_dir="$RESULTS_HOME/$CFG/train" \
+#     --eval_dir="$RESULTS_HOME/$CFG/eval" \
+#     --batch_size=$BS \
+#     --model_config="$MDL_CFGS/$CFG/train.json" \
+#     --eval_interval_secs=1800 \
+#     --sequence_length=$SEQ_LEN &
